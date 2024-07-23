0ae4bac4e40b83e124e754a0ef2db32a41fd6417
dndeveloper-saurabh <saurabh@pustack.com>
2024-07-23 18:51:01 +0530

    fix: reset few values

--- src/components/SlateEditor/SlateEditor.tsx
+++ src/components/SlateEditor/SlateEditor.tsx
@@ -44,35 +44,7 @@ const initialValue: CustomElement[] = [
     align: "left",
     children: [
       {
-        text: 'The editor gives you full control over the logic you can add. For example, it\'s fairly common to want to add markdown-like shortcuts to editors. So that, when you start a line with "> " you get a blockquote that looks like this:',
-      },
-    ],
-  },
-  {
-    type: "block-quote",
-    align: "left",
-    children: [{ text: "A wise quote." }],
-  },
-  {
-    type: "paragraph",
-    align: "left",
-    children: [
-      {
-        text: 'Order when you start a line with "## " you get a level-two heading, like this:',
-      },
-    ],
-  },
-  {
-    type: "heading-two",
-    align: "left",
-    children: [{ text: "Try it out!" }],
-  },
-  {
-    type: "paragraph",
-    align: "left",
-    children: [
-      {
-        text: 'Try it out for yourself! Try starting a new line with ">", "-", or "#"s.',
+        text: "",
       },
     ],
   },
