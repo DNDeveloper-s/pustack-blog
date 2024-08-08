9f482f1152d0a5c016fbe6be172d2e4718d01163
dndeveloper-saurabh <saurabh@pustack.com>
2024-08-08 11:30:19 +0530

    feat: venue support added

--- src/components/Events/EventDetailPage.tsx
+++ src/components/Events/EventDetailPage.tsx
@@ -585,28 +585,47 @@ export default function EventDetailPage({ _event }: { _event?: DocumentData }) {
             </div>
           </div>
           <div className="mt-5">
-            <div className="flex cursor-pointer items-center before:absolute relative before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:bg-opacity-60 overflow-hidden before:z-[2] border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-              <Image
-                className="w-full aspect-video rounded-2xl"
-                width={1000}
-                height={1000}
-                src="https://content.jdmagicbox.com/comp/ernakulam/m4/0484px484.x484.140206113128.a9m4/catalogue/we-create-events-panampilly-nagar-ernakulam-event-management-companies-nsobpzm660.jpg"
-                alt="Nothing"
-              />
-              <div className="absolute bottom-3 md:bottom-5 w-full z-10 px-3 md:px-8 flex items-center justify-between">
-                <div className="text-white">
-                  <h3 className="font-featureHeadline text-[18px] md:text-[25px]">
-                    Courtyard, Downtown
-                  </h3>
-                  <p className="text-white text-[12px] md:text-[16px] text-opacity-40">
-                    Open in maps
-                  </p>
-                </div>
-                <div className="w-10 h-10 text-xl text-gray-400 border border-gray-500 rounded-full bg-gray-700 flex items-center justify-center">
-                  <IoMdMap />
+            {event.venue.type === "offline" ? (
+              <div className="flex cursor-pointer items-center before:absolute relative before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:bg-opacity-60 overflow-hidden before:z-[2] border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
+                <AppImage
+                  className="w-full aspect-video rounded-2xl"
+                  width={1000}
+                  height={1000}
+                  src={event.venue.image}
+                  alt="Nothing"
+                />
+                <div className="absolute bottom-3 md:bottom-5 w-full z-10 px-3 md:px-8 flex items-center justify-between">
+                  <div className="text-white">
+                    <h3 className="font-featureHeadline text-[18px] md:text-[25px]">
+                      {event.venue.name}
+                    </h3>
+                    <p className="text-white text-[12px] md:text-[16px] text-opacity-40">
+                      Open in maps
+                    </p>
+                  </div>
+                  <div className="w-10 h-10 text-xl text-gray-400 border border-gray-500 rounded-full bg-gray-700 flex items-center justify-center">
+                    <IoMdMap />
+                  </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              <div className="p-4 border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
+                <div className="flex-1">
+                  <p className="text-sm text-appBlack text-opacity-60">
+                    Meet link:
+                  </p>
+                  <Link
+                    href={event.venue.link}
+                    target="_blank"
+                    className="text-appBlue"
+                  >
+                    <b>
+                      <u>{event.venue.link}</u>
+                    </b>
+                  </Link>
+                </div>
+              </div>
+            )}
           </div>
 
           <div className="mt-8" id={"contact"}>
